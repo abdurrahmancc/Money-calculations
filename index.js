bc3b03e3fea2ff32f22dcf5c276c5317fc1178e6
abdurrahmancc <personalbusinessmail63@gmail.com>
2022-02-17 05:30:54 +0600

    helf clear add

--- index.js
+++ index.js
@@ -2,19 +2,24 @@ const income = document.getElementById("income");
 const food = document.getElementById("food");
 const rent = document.getElementById("rent");
 const clothes = document.getElementById("clothes");
+
+// button calculate addEventListener
 document.getElementById("btn-Calculate").addEventListener("click", function () {
   const totalExpenses = document.getElementById("totalExpenses");
   const balance = document.getElementById("balance");
 
   // total cost
-  const totalCost = totalExpensesCost();
+  const totalCost = expensesTotal();
   console.log(totalCost);
   totalExpenses.innerText = totalCost;
   balance.innerText = parseInt(income.value) - totalCost;
+
+  //   Remaining Balance
+  const remainingBalance = document.getElementById("remaining-Balance");
+  remainingBalance.innerText = parseInt(income.value) - totalCost;
 });
 
 // save section
-
 document.getElementById("btn-save").addEventListener("click", function () {
   const savingAmount = document.getElementById("saving-Amount");
   const saveInput = document.getElementById("save-Input");
@@ -22,11 +27,12 @@ document.getElementById("btn-save").addEventListener("click", function () {
   const save = (parseInt(income.value) * parseInt(saveInput.value)) / 100;
   console.log(save);
   savingAmount.innerText = save;
-  const totalCost = totalExpensesCost();
+  const totalCost = expensesTotal();
   remainingBalance.innerText = parseInt(income.value) - (totalCost + save);
 });
 
-function totalExpensesCost() {
+// Expenses Total calculation
+function expensesTotal() {
   const cost = parseInt(food.value) + parseInt(rent.value) + parseInt(clothes.value);
   return cost;
 }
